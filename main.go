dfcc7753916cf9a049c004942c52f81fb50b7a8d
oktetobot <oktetobot@users.noreply.github.com>
2025-06-17 14:18:54 +0000

    Update hello message to showcase Okteto's cloud native development capabilities

--- main.go
+++ main.go
@@ -14,5 +14,5 @@ func main() {
 }
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+	fmt.Fprint(w, "🚀 Deployed with Okteto - Cloud Native Development Made Easy! ☁️")
 }
